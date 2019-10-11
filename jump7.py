45b3ce97802a26d6383537c1aa5e0616e78f77b3
caojun <caojun3373334544@qq.com>
2019-10-11 22:44:28 +0800

    ok

---
+++ jump7.py
@@ -0,0 +1,15 @@
+git add (文件名/目录名）   (或 --all)
+git config --global user. name 'yourname'
+git config --global user. email'yourEmill@xx.com'
+git commit -m '注释'
+git remote add origin （仓库链接）
+git push origin master
+   
+
+
+echo "# -shiyanlou-code" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/1MyWorld/-shiyanlou-code.git
+git push -u origin master
